12b934b38e729cc2b28ab01aa098ed0a3b54bd17
JorgeX2304 <jorge123.fugadegas@gmail.com>
2025-09-25 12:52:00 -0400

    archivos de Proyexto en unity

---
+++ ProyectoUnity/Assets/Scripts/texto/timer.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText; 
+    public int startTime = 30;        
+    private int currentTime;
+
+    public CameraMover cameraMover;
+
+    private Coroutine countdownCoroutine;
+
+    void Start()
+    {
+        ResetCountdown();
+    }
+
+    IEnumerator StartCountdown()
+    {
+        while (currentTime > 0)
+        {
+            timerText.text = currentTime.ToString();
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+        }
+
+        // Cuando llegue a 0
+        timerText.text = "0";
+        Debug.Log("¡Tiempo terminado!");
+
+        if (cameraMover != null)
+        {
+            cameraMover.StartMoving();
+        }
+    }
+
+    // 🔹 Método para reiniciar el contador
+    public void ResetCountdown()
+    {
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+        }
+
+        currentTime = startTime;
+        countdownCoroutine = StartCoroutine(StartCountdown());
+    }
+}
