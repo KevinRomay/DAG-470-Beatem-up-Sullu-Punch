e101322d7b298bd4befda1a4fedd124cf7778962
DiegoFdez017 <133017537+DiegoFdez017@users.noreply.github.com>
2025-11-03 18:00:51 -0400

    Se realizo una reparación global del UI
    -Implementación del UI según diagrama mostrado en clases.

---
+++ Assets/Scripts/UI/CambiarEscena.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour
+{
+    public void IrAlMenuPrincipal()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+    public void IrAModoDeJuego()
+    {
+        SceneManager.LoadScene("ModoDeJuego");
+    }
+    public void IrANiveles()
+    {
+        SceneManager.LoadScene("Niveles");
+    }
+    public void IrASeleccionarPersonaje()
+    {
+        SceneManager.LoadScene("SeleccionPersonajes");
+    }
+    public void IrAPantallaCarga()
+    {
+        SceneManager.LoadScene("PantallaDeCarga");
+    }
+}
---
+++ Assets/Scripts/UI/PantallaCargaController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PantallaCargaController : MonoBehaviour
+{
+    public string escenaDestino = "GameScene"; // Nombre exacto de la escena siguiente
+    public float tiempoEspera = 5f; // segundos
+
+    void Start()
+    {
+        StartCoroutine(CargarDespuesDeTiempo());
+    }
+
+    IEnumerator CargarDespuesDeTiempo()
+    {
+        yield return new WaitForSeconds(tiempoEspera);
+        SceneManager.LoadScene(escenaDestino);
+    }
+}
+
